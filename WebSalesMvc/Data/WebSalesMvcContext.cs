c3b42529de601e875ccee8545920b00cc8ad96fe
Fabio Matias <fbiomat@hotmail.com>
2022-06-15 12:32:32 -0300

    Bootstrap changed

---
+++ WebSalesMvc/Data/WebSalesMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebSalesMvc.Models
+{
+    public class WebSalesMvcContext : DbContext
+    {
+        public WebSalesMvcContext (DbContextOptions<WebSalesMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebSalesMvc.Models.Department> Department { get; set; }
+    }
+}
