9cb3c4084287fdccc74b7472156c3a07e984fed5
Fabio Matias <fbiomat@hotmail.com>
2022-06-25 17:22:05 -0300

    Update seller and custom service exception

---
+++ WebSalesMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace WebSalesMvc.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message)
+        {
+        }
+
+    }
+    
+
+    
+}
